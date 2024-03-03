465cd54e11d619cf7b8428084c28ccec2befc6fd
MarroosSK <maros1chmel@gmail.com>
2024-03-03 12:34:02 +0100

    4 - correct page to stripe

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -24,8 +24,8 @@ export const getStripeSession = async ({
       address: "auto",
       name: "auto",
     },
-    success_url: `${domainUrl}/payment/success`,
-    cancel_url: `${domainUrl}/payment/cancelled`,
+    success_url: `https://note-site-app.vercel.app/payment/success`,
+    cancel_url: `https://note-site-app.vercel.app/payment/cancelled`,
   });
 
   return session.url as string;
